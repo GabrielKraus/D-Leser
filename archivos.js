6a8b8701acaf10e08bbcd2422400a8f159ccb4a6
GabrielKraus <gabrielalvarezkraus@gmail.com>
2022-06-07 22:11:02 -0300

    first commit

--- archivos.js
+++ archivos.js
@@ -1,87 +1,114 @@
-const fs = require('fs');
+const express = require('express')
+const app = express()
+const PORT = 8080
+const server = app.listen(PORT, () => {
+    console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
+})
+server.on("error", error => console.log(`Error en servidor ${error}`))
+
+
+const fs = require("fs");
 class Contenedor {
     constructor(nombre) {
-        this.nombre = nombre
+        // podemos simplificar esto, y solo llamar a this.nombre
+        this.nombre = `./${nombre}`;
     }
-    static id = 0
-    static productArray = []
-    save(obj) {
-        Contenedor.id++
-        obj['id'] = Contenedor.id
-        Contenedor.productArray.push(obj)
-        fs.writeFile(`./${this.nombre}`, JSON.stringify(Contenedor.productArray, null, 2), error => {
-            if (error) {
-                console.log("error")
+    static id = 0;
+    async save(obj) {
+        try {
+            const productos = await this.getAll();
+            if (productos.length === 0) {
+                obj.id = 1;
             } else {
-                console.log(`La id del producto ${obj['title']} es: ${obj['id']}`)
+                obj.id = productos[productos.length - 1].id + 1;
             }
-        })
-        return obj['id']
+            productos.push(obj);
+
+            await fs.promises.writeFile(
+                this.nombre,
+                JSON.stringify(productos, null, 2)
+            );
+        } catch (error) {
+            console.log(error);
+        }
     }
     getById(id) {
-        fs.readFile(`./${this.nombre}`, (error, res) => {
+        fs.readFile(this.nombre, (error, res) => {
             if (error) {
-                console.log("error")
+                console.log("error");
             } else {
-                let products = JSON.parse(res)
-                console.log(products.find(product => product.id == id))
+                let products = JSON.parse(res);
+                console.log(products.find((product) => product.id == id));
             }
-        })
+        });
     }
-    getAll() {
-        fs.readFile(`./${this.nombre}`, (error, res) => {
-            if (error) {
-                console.log("error")
-            } else {
-                console.log(JSON.parse(res))
-            }
-        })
+    async getAll() {
+
+        try {
+            const productos = await fs.promises.readFile(this.nombre);
+            return JSON.parse(productos);
+        } catch (error) {
+
+            console.log(error);
+            return [];
+        }
+
     }
     deleteById(id) {
-        fs.readFile(`./${this.nombre}`, (error, res) => {
+        fs.readFile(this.nombre, (error, res) => {
             if (error) {
-                console.log("error")
+                console.log("error");
             } else {
-                let products = JSON.parse(res)
-                const index = products.findIndex(product => {
+                let products = JSON.parse(res);
+                const index = products.findIndex((product) => {
                     return product.id == id;
                 });
-                Contenedor.productArray.splice(index, 1)
-                fs.writeFile(`./${this.nombre}`, JSON.stringify(Contenedor.productArray, null, 2), error => {
-                    if (error) {
-                        console.log("error")
+                Contenedor.productArray.splice(index, 1);
+                fs.writeFile(
+                    this.nombre,
+                    JSON.stringify(Contenedor.productArray, null, 2),
+                    (error) => {
+                        if (error) {
+                            console.log("error");
+                        }
                     }
-                })
+                );
             }
-        })
+        });
     }
     deleteAll() {
         fs.readFile(`./${this.nombre}`, (error, res) => {
             if (error) {
-                console.log("error")
+                console.log("error");
             } else {
-                fs.writeFile(`./${this.nombre}`, JSON.stringify([]), error => {
+                fs.writeFile(`./${this.nombre}`, JSON.stringify([]), (error) => {
                     if (error) {
-                        console.log("error")
+                        console.log("error");
                     }
-                })
+                });
             }
-        })
+        });
     }
 }
+let archivo = new Contenedor("productos.txt");
+const producto = {
+    title: "producto",
+    price: 200,
+    thumbnail: "https://as01.epimg.net/meristation/imagenes/2022/05/19/noticias/1652944408_523527_1652944740_noticia_normal.jpg",
+};
+archivo.save(producto).then()
 
+app.get('/', (req, res) => {
+    res.send("<a href='/productos'>Ver todos los productos</a> <br> <a href='/productoRandom'>ver un producto aleatorio</a>")
+})
 
-let archivo = new Contenedor("productos.txt")
+let allProducts = []
+archivo.getAll().then(res=>  allProducts = res )
 
-archivo.save({ title: "producto1", price: 50, thumbnail: "https://i0.wp.com/codigoespagueti.com/wp-content/uploads/2020/12/The-Mandalorian-Grogu.jpg?fit=1280%2C720&quality=80&ssl=1" })
-archivo.save({ title: "producto2", price: 100, thumbnail: "https://img.europapress.es/fotoweb/fotonoticia_20220216180422_420.jpg" })
-archivo.save({ title: "producto3", price: 200, thumbnail: "https://as01.epimg.net/meristation/imagenes/2022/05/19/noticias/1652944408_523527_1652944740_noticia_normal.jpg" })
-
-
-// arch.getAll()
-
-archivo.deleteAll()
-
-
-
+app.get('/productos', (req, res) => {
+    res.send( allProducts )
+})
+app.get('/productoRandom', (req, res) => {
+    res.send( allProducts[Math.floor(Math.random() * (allProducts.length))] )
+})
 
