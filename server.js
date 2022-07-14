6a775c2ff2fa7b63bfcbe803a75a7e8656cd78ab
GabrielKraus <gabrielalvarezkraus@gmail.com>
2022-07-14 00:44:57 -0300

    primera entrega

--- server.js
+++ server.js
@@ -17,113 +17,41 @@ app.use('/api/productos', routerProductos);
 app.use('/api/carrito', routerCarritos);
 app.use(express.static("public"))
 app.use(express.urlencoded({ extended: true }))
-
-
-class Contenedor {
-    constructor(nombre) {
-        this.nombre
-    }
-    // async  getAll(archivo) {
-    //     try {
-    //         const datos = await fs.promises.readFile(archivo);
-    //         return datos;
-    //     } catch (error) {
-    //         console.log(error);
-    //         return [];
-    //     }
-    // }
-    // save(obj) {
-    //     const datos =  this.getAll();
-    //     if (datos.length === 0) {
-    //         obj.id = 1;
-    //     } else {
-    //         obj.id = datos[datos.length - 1].id + 1;
-    //     }
-    //     datos.push(obj);
-    //     fs.writeFile(
-    //         this.nombre,
-    //         JSON.stringify(datos, null, 2)
-    //     );
-    // }
-}
-
-let archivoProductos = new Contenedor("productos.txt")
-let archivoCarrito = new Contenedor("carrito")
-
-
-
-
 //#region Productos
-class Product {
-    constructor(title, price, thumbnail, timeStamp, id) {
-        this.title = title,
-            this.price = price,
-            this.thumbnail = thumbnail,
-            this.timeStamp = timeStamp
-        this.id = id
-    }
-}
-
-// const ListaProductos = [{
-//     title: "Libro1",
-//     price: 150,
-//     thumbnail: "foto",
-//     timeStamp: Date.now,
-//     id: 1
-// }, {
-//     title: "Libro2",
-//     price: 250,
-//     thumbnail: "foto",
-//     id: 2
-// }
-// ];
-
-
-
-
 routerProductos.get("/", (req, resPost) => {
-    // resPost.json(fs.promises.readFile(archivoProductos))
-
-    // fs.readFile("./productos.txt", (error, res) => {
-    //     resPost.json(res)
-    // })
-
     fs.readFile("./productos.txt", 'utf8', (err, data) => {
         if (err) {
             console.log("error")
         }
-        resPost.json(data)
+        resPost.json( JSON.parse(data))
     });
 })
 
 
 
-// routerProductos.get("/:id", (req, res) => {
-//     let id = parseInt(req.params.id)
-//     let producto = productos.find(producto => producto.id == id)
-//     if (producto != null) {
-//         res.json(
-//             producto
-//         )
-//     } else {
-//         res.send({ error: `no existe el producto con el id ${id}` })
-//     }
-// })
-
-routerProductos.post("/", (req, res) => {
-    ListaProductos = []
+routerProductos.get("/:id", (req, res) => {
+    let id = parseInt(req.params.id)
     fs.readFile("./productos.txt", 'utf8', (err, data) => {
         if (err) {
             console.log("error")
         }
-        ListaProductos = data
+        let productos = JSON.parse(data)
+        let producto = productos.find(producto => producto.id == id)
+        res.json(producto)
     });
+})
+
+
+let ListaProductos = [];
+routerProductos.post("/", (req, res) => {
     const newProduct = {
         title: req.body.title,
         price: req.body.price,
-        thumbnail: req.body.thumbnail
+        thumbnail: req.body.thumbnail,
+        timeStamp: new Date(Date.now()).toLocaleString(),
+        id: ListaProductos.length
     };
-    // productList.push(newProduct)
+    ListaProductos.push(newProduct)
     fs.writeFile("./productos.txt", JSON.stringify(ListaProductos, null, 2), err => {
         if (err) {
             console.error(err);
@@ -149,13 +77,26 @@ routerProductos.post("/", (req, res) => {
 // })
 
 
-// routerProductos.delete("/:id", (req, res) => {
-//     let productId = parseInt(req.params.id)
-//     productos.splice(productos.indexOf(productos.find(el => el.id == productId)), 1)
-//     res.json({
-//         productos
-//     })
-// })
+routerProductos.delete("/:id", (req, res) => {
+    let id = parseInt(req.params.id)
+
+    fs.readFile("./productos.txt", 'utf8', (err, data) => {
+        if (err) {
+            console.log("error")
+        }
+        let productos = JSON.parse(data)
+        let producto = productos.find(producto => producto.id == id)
+
+        productos.splice(productos.indexOf(producto), 1)
+
+        fs.writeFile("./productos.txt", JSON.stringify(productos, null, 2), err => {
+            if (err) {
+                console.error(err);
+            }
+            res.json(productos)
+        });
+    });
+})
 
 
 //#endregion
