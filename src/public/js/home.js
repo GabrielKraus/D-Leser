da0e77c9a2ea7c85b4195e19167c2d6303163062
Gabriel Kraus <93491829+GabrielKraus@users.noreply.github.com>
2022-10-25 15:26:41 -0300

    Update home.js

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -3,7 +3,6 @@ const btn = document.getElementById('logoutbtn')
 
 btn.addEventListener('click',evt=>{
     fetch('/api/sessions/logout').then(result=>result.json()).then(json=>console.log(json));
-    location.href = `http://localhost:${config.server.PORT}/`;
 })
 
 const form = document.getElementById('agregarProductoForm');
